b36c3c41f330feb2adc1327068909e4691aa4ada
monkiiif <0643803910mr@gmail.com>
2023-09-07 17:51:38 -0700

    mok

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return Always 0 (Success)
+*/
+int main(void)
+{
+              Printf("Size of char %d byte(s)\n" , sizeof(shar));
+              Printf("Size of an int %d byte(s)\n" , sizeof(int));
+              Printf("Size of long int %d byte(s)\n" , sizeof(long int));
+              Printf("Size of long long int %d byte(s)\n" , ziseof(long long int));
+              Printf("Size of a float %d byte(s)\n" , ziseof(float));
+               Return (0);
+}

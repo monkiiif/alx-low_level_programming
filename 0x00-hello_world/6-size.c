798a76b19ec84ef423da637af0c78d08b539e822
monkiiif <0643803910mr@gmail.com>
2023-09-07 17:54:05 -0700

    mok

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,20 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-* main - Entry point
-*
-* Return Always 0 (Success)
-*/
+ * main - A program that prints various computer types
+ * Return: 0(Success)
+ */
 int main(void)
 {
-              Printf("Size of char %d byte(s)\n" , sizeof(shar));
-              Printf("Size of an int %d byte(s)\n" , sizeof(int));
-              Printf("Size of long int %d byte(s)\n" , sizeof(long int));
-              Printf("Size of long long int %d byte(s)\n" , ziseof(long long int));
-              Printf("Size of a float %d byte(s)\n" , ziseof(float));
-               Return (0);
+char a;
+int b;
+long int c;
+long long int d;
+float e;
+
+printf("size of a char: %lu byte(s)\n", sizeof(a));
+printf("size of int: %lu byte(s)\n", sizeof(b));
+printf("size of a long int: %lu byte(s)\n", sizeof(c));
+printf("size of a long long int: %lu byte(s)\n", sizeof(d));
+printf("size of a float: %lu byte(s)\n", sizeof(e));
+return (0);
 }

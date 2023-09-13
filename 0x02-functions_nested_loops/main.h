fa03a9f10c49da61fc4bf69a69658b92a0491ef4
monkiiif <0643803910mr@gmail.com>
2023-09-13 06:21:10 -0700

    mok

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,3 @@
-#ifndef main_h
-#define main_h
 
 int _putchar(char);
 void print_alphabet(void);
@@ -14,6 +12,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 void print_times_table(int n);
-
-#endi
 
